4def8f51146360ac5f9fbbff8387a3a80ee64687
vovapasko <vovapasko1699@gmail.com>
2023-11-12 16:11:29 +0100

    initial commit

---
+++ src/main/java/org/example/Scanner.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.List;
+
+public class Scanner {
+    public Scanner(String source) {
+    }
+
+
+    public List<Token> scanTokens() {
+        return null;
+    }
+}
